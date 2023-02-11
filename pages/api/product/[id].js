a93ab4589e66c7206b49120cb762dee12d883fb0
Kyawzint17 <u6228105@au.edu>
2023-02-11 20:19:10 +0700

    product update

--- pages/api/product/[id].js
+++ pages/api/product/[id].js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   const fileContents = await fs.readFile(jsonDirectory + "/products.json", "utf8");
   const products = JSON.parse(fileContents);
 
-  const p = products.find((product) => product.id == req.query.id);
+  const p = products.find((product) => product.id == Number(req.query.id));
   console.debug(req.query.id, p);
 
   if (!p) {
